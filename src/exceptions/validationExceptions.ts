35d4d8baa8354a1cdaf6d372be40d4dce5694c0b
pauldepstein <pauldepstein@yahoo.com>
2025-01-12 10:57:08 +0000

    Add file for exceptions in parameter validations

---
+++ src/exceptions/validationExceptions.ts
@@ -0,0 +1,41 @@
+/**
+ * ValidationError class that extends the built-in Error class.
+ * 
+ * This class provides an error with conditional logging to the console.
+ */
+class ValidationError extends Error {
+    /**
+     * A flag that determines whether the error should be logged.
+     */
+    private logging: boolean;
+  
+    /**
+     * Creates an instance of ValidationError.
+     * 
+     * @param message - The error message.
+     * @param logging - A boolean flag indicating whether the error should be logged. Defaults to `true`.
+     */
+    constructor(message: string, logging: boolean = true) {
+      super(message);  
+      this.name = this.constructor.name;
+      this.logging = logging;
+  
+    // Log to console if not using a browser
+    if (this.logging) {
+            console.error(this.toString());  
+        }
+  
+    }
+  
+    /**
+     * Returns a string representation of the error.
+     * 
+     * @returns A string containing the name and message of the error.
+     */
+    toString(): string {
+      return `${this.name}: ${this.message}`;
+    }
+  }
+  
+
+  

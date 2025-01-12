adb84a4bde49f0d7b2f96f84a6c075c8a988a05c
pauldepstein <pauldepstein@yahoo.com>
2025-01-12 10:35:51 +0000

    Comments added

--- src/constants.ts
+++ src/constants.ts
@@ -1,1 +1,4 @@
+//Contains constants used throughout the application
+
+//Calendar Constants
 export const MONTHS_IN_YEAR = 12;
--- src/exceptions/errorStringHandling.ts
+++ src/exceptions/errorStringHandling.ts
@@ -1,3 +1,9 @@
+/**
+ * Returns the error message for invalid input
+ * @param {string} errorParam -- the parameter that caused the error
+ * @param {string} errorMessage -- the error message for the parameter
+ * @returns {string} -- the final error message
+ */
 function displayError(errorParam: string, errorMessage: string): string {
     const ERROR_INDICATOR = "Error: ";
     const errorString = `${ERROR_INDICATOR}${errorParam}: \n${errorMessage}`;

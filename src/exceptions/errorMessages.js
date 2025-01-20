7357c4971167d093ff2588535c68ac11b6ee17ea
pauldepstein <pauldepstein@yahoo.com>
2025-01-20 18:49:15 +0000

    Delete src/exceptions/errorMessages.js
    All js files deleted

--- src/exceptions/errorMessages.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.ERROR_MESSAGES = void 0;
-exports.ERROR_MESSAGES = {
-    UNEXPECTED_NEGATIVE: "Input should be at least 0.",
-    UNEXPECTED_NON_POSITIVE: "Input must be positive.",
-    UNEXPECTED_FRACTION: "Input must be a whole number.",
-};
